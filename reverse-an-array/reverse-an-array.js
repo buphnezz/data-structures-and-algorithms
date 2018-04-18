c1c15e13de0b75f7166ab01b769deb2afa3cc375
zachary schumpert <buphnezz@gmail.com>
2018-04-17 20:54:54 -0700

    forgot to add code to reverse-an-array.js!  LOL...done!

---
+++ reverse-an-array/reverse-an-array.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let reverseArray = [1, 2, 3, 4];
+
+reverseArray = reverseArray.split;
+
+for ( i = 0; i < reverseArray.length; i++) {
+  reverseArray.join;
+}
+
+return reverseArray;
