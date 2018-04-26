d5d2ae3533f97925ab51b868e9d1020c40ebccff
Zachary Schumpert <buphnezz@gmail.com>
2018-04-25 21:59:44 -0700

    Update ll-merge-test.js

--- ll-merge/__test__/ll-merge-test.js
+++ ll-merge/__test__/ll-merge-test.js
@@ -1,1 +1,11 @@
 
+'use strict';
+
+const LinkedList = require('../ll-merge/ll-merge');
+
+describe('linked-list.js', () => {
+  test('#constructor', () => {
+    const testList = new LinkedList();
+    expect(testList.head).toBeNull();
+  });
+});
