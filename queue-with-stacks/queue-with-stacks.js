d1f557007d41d15dcd3dda65fcf2a429e0f9c84f
zachary schumpert <buphnezz@gmail.com>
2018-05-01 08:56:26 -0700

    whiteboard 10 needs work

---
+++ queue-with-stacks/queue-with-stacks.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const stackOne = {a, b, c, d};
+const stackTwo = {e, f, g, h};
+
+
+class Queue {
+  constructor() {
+    this.stackOne.pop(),
+    this.stackTwo.pop(),
+  }
+  while (stackOne !== undefined && stackTwo !== undefined) {
+    Queue.constructor.enque()
+  }
+  }
+  constructor();
+};
