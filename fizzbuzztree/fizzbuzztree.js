f21568ddce10333a3f606a9d218d426005f596de
zachary schumpert <buphnezz@gmail.com>
2018-05-09 15:52:59 -0700

    whiteboard is complete, just need to add tests

--- fizzbuzztree/fizzbuzztree.js
+++ fizzbuzztree/fizzbuzztree.js
@@ -1,11 +1,32 @@
-'use strict'
+'use strict';
 
-function FizzBuzz(tree) {
-  if (FizzBuzz[i] === FizzBuzz[i].modulus(15)){
-    FizzBuzz[i] = 'FizzBuzz'};
-  if (FizzBuzz[i] === FizzBuzz[i].modulus(5)){
-    FizzBuzz[i] = 'Fizz'};
-  if (FizzBuzz[i] === FizzBuzz[i].modulus(3)){
-    FizzBuzz[i] = 'Buzz'};
-}
+let stringedPreOrder = '';
 
+const preOrderTraversal = (rootNode) => {
+
+  if (!rootNode) {
+    return undefined;
+  }
+  if (rootNode.value % 3 === 0 && rootNode.value % 5 === 0) {
+    rootNode.value = 'FizzBuzz';
+  }
+  if (rootNode.value % 3 === 0) {
+    rootNode.value = 'Fizz';
+  }
+  if (rootNode.value % 5 === 0) {
+    rootNode.value = 'Buzz';
+  }
+  stringedPreOrder += `,${rootNode.value}`;
+  preOrderTraversal(rootNode.left); 
+  preOrderTraversal(rootNode.right);
+  return stringedPreOrder.replace(/^,/, '');
+};
+
+const fizzBuzzTree = (tree) => {
+  if (!tree.root) {
+    return undefined;
+  }
+  return preOrderTraversal(tree.root);
+};
+
+export default fizzBuzzTree;
