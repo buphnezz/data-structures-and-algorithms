65c8dbc3da22ee56418c0499c7f6295d79757574
zachary schumpert <buphnezz@gmail.com>
2018-05-09 19:49:50 -0700

    Whiteboard solution working, all tests passing, resubmit complete!

--- fizzbuzztree/fizzbuzztree.js
+++ fizzbuzztree/fizzbuzztree.js
@@ -1,31 +1,20 @@
 'use strict';
 
-let stringedPreOrder = '';
-
-const preOrderTraversal = (rootNode) => {
+const fizzBuzzTree = (rootNode) => {
   if (!rootNode) {
     return undefined;
   }
   if (rootNode.value % 3 === 0 && rootNode.value % 5 === 0) {
     rootNode.value = 'FizzBuzz';
-  }
-  if (rootNode.value % 3 === 0) {
+  } else if (rootNode.value % 3 === 0) {
     rootNode.value = 'Fizz';
-  }
-  if (rootNode.value % 5 === 0) {
+  } else if (rootNode.value % 5 === 0) {
     rootNode.value = 'Buzz';
-  }
-  stringedPreOrder += `,${rootNode.value}`;
-  preOrderTraversal(rootNode.left); 
-  preOrderTraversal(rootNode.right);
-  return stringedPreOrder.replace(/^,/, '');
-};
+  } 
 
-const fizzBuzzTree = (tree) => {
-  if (!tree.root) {
-    return undefined;
-  }
-  return preOrderTraversal(tree.root);
+  fizzBuzzTree(rootNode.left); 
+  fizzBuzzTree(rootNode.right);
+  return undefined;
 };
 
 export default fizzBuzzTree;
